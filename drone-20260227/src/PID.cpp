99f11bd23807aa50576293eb33911181c3e26d0f
KOT4T2U <yeon032323@hanyang.ac.kr>
2026-02-27 17:29:24 +0900

    update

--- drone-20260227/src/PID.cpp
+++ drone-20260227/src/PID.cpp
@@ -3,33 +3,3 @@
 
 #include <Arduino.h>
 #include "PID.h"
-
-float calcPID_Roll(float targetRoll, float currentRoll) {   
-    error = targetRoll - currentRoll;
-    P_error = error;
-    I_error += error * DT;
-    D_error = (error - P_error) / DT;
-
-    P_out = R_P * P_error;
-    I_out = R_I * I_error;
-    D_out = R_D * D_error;
-
-    return P_out + I_out + D_out;
-}
-
-float calcPID_Pitch(float targetPitch, float currentPitch) {
-    error = targetPitch - currentPitch;
-    P_error = error;
-    I_error += error * DT;
-    D_error = (error - P_error) / DT;
-
-    P_out = P_P * P_error;
-    I_out = P_I * I_error;
-    D_out = P_D * D_error;
-
-    return P_out + I_out + D_out;
-}
-
-float calcPID_Yaw(float targetYaw, float currentYaw) {
-    error = targetYaw - currentYaw;
-    P_error = error;
