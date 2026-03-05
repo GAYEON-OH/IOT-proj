53df87712bab3265186c5d91cb4fcba71c4b5fb6
NamHyeju <hyeju31@ajou.ac.kr>
2026-03-05 16:47:27 +0900

    update

--- drone-20260227/include/PID.h
+++ drone-20260227/include/PID.h
@@ -1,16 +1,12 @@
+#ifndef PID_H
+#define PID_H
+
 #include "config.h"
 
-float error = 0.0f, P_error = 0.0f, I_error = 0.0f, D_error = 0.0f;
-float P_out = 0.0f, I_out = 0.0f, D_out = 0.0f;
+void initPID();
 
-void initPID(){
-    
-}
-float calcPID_Roll(float targetRoll, float currentRoll) {
-}
+float calcPID_Roll(float targetRoll, float currentRoll, float throttle);
+float calcPID_Pitch(float targetPitch, float currentPitch, float throttle);
+float calcPID_Yaw(float targetYaw, float currentYaw, float throttle);
 
-float calcPID_Pitch(float targetPitch, float currentPitch) {
-}
-
-float calcPID_Yaw(float targetYaw, float currentYaw) {
-}
+#endif
--- drone-20260227/src/main.cpp
+++ drone-20260227/src/main.cpp
@@ -9,7 +9,6 @@
 #include "IMU.h"
 #include "RX.h"
 #include "PID.h"
-#include "Motor.h"
 
 
 MPU6050 mpu;
@@ -21,6 +20,7 @@ void setup() {
   initIMU();
   initPID();
   initMotors(); 
+  initRX();
 }
 
 void loop() {
@@ -33,16 +33,13 @@ void loop() {
   readIMU();
   calcRP();
 
-  float target_Throttle = 0.4f;  // 40% 부양
-    float target_Roll = 0.0f;      // 좌우 중립  
-    float target_Pitch = 0.0f;     // 앞뒤 중립
-    float target_Yaw = 0.0f;
+  readBluetooth();
     
-    float current_Roll = Roll, current_Pitch = Pitch, current_Yaw = Yaw;
+  float current_Roll = Roll, current_Pitch = Pitch, current_Yaw = Yaw;
     
-    float PID_Roll = calcPID_Roll(target_Roll, current_Roll);
-    float PID_Pitch = calcPID_Pitch(target_Pitch, current_Pitch);
-    float PID_Yaw = calcPID_Yaw(target_Yaw, current_Yaw);
+  float PID_Roll = calcPID_Roll(target_Roll, current_Roll,base_Throttle);
+  float PID_Pitch = calcPID_Pitch(target_Pitch, current_Pitch,base_Throttle);
+  float PID_Yaw = calcPID_Yaw(target_Yaw, current_Yaw,base_Throttle);
     
-    calcMotors(target_Throttle, PID_Roll, PID_Pitch, PID_Yaw);
+  calcMotors(base_Throttle, PID_Roll, PID_Pitch, PID_Yaw);
 }
