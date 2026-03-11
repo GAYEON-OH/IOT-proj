a4ed0281e33449ca46d8bc5ae0b5b6fb943dab0c
HY-OGY <yeon032323@hanyang.ac.kr>
2026-03-11 09:56:34 +0900

    Squashed commit of the following:
    commit e0e76ff7aef8ba8c921f1b68bc445e805e45fbab
    Merge: d0ee2e5 a7fd78a
    Author: HY-OGY <yeon032323@hanyang.ac.kr>
    Date:   Thu Mar 5 17:10:45 2026 +0900
    
        Merge branch 'OGY_BR' into develop
    
    commit d0ee2e542a4d4c0bcf02529d3b1aee5716afeb3c
    Merge: abba617 53df877
    Author: HY-OGY <yeon032323@hanyang.ac.kr>
    Date:   Thu Mar 5 17:06:48 2026 +0900
    
        Merge pull request #5 from GAYEON-OH/NHJ-BR
    
        Nhj br
    
    commit 53df87712bab3265186c5d91cb4fcba71c4b5fb6
    Author: NamHyeju <hyeju31@ajou.ac.kr>
    Date:   Thu Mar 5 16:47:27 2026 +0900
    
        update

--- drone-20260227/src/PID.cpp
+++ drone-20260227/src/PID.cpp
@@ -1,23 +1,26 @@
 #include "config.h"
 #include <Arduino.h>
-
-float error = 0.0f, P_error = 0.0f, I_error = 0.0f, D_error = 0.0f;
-float P_out = 0.0f, I_out = 0.0f, D_out = 0.0f;
+#include "PID.h"
 
 /* 각 축별 적분값 (static으로 함수 간 유지) */
 static float roll_int = 0.0f, pitch_int = 0.0f, yaw_int = 0.0f;
 static float roll_prev = 0.0f, pitch_prev = 0.0f, yaw_prev = 0.0f;
-static unsigned long last_time = 0;
+static unsigned long last_time_roll = 0;
+static unsigned long last_time_pitch = 0;
+static unsigned long last_time_yaw = 0;
 
 void initPID() {
     roll_int = pitch_int = yaw_int = 0.0f;  // 적분값 초기화
     roll_prev = pitch_prev = yaw_prev = 0.0f; // 이전 오차 초기화
 }
 
-float calcPID_Roll(float targetRoll, float currentRoll) {
+float calcPID_Roll(float targetRoll, float currentRoll,float throttle) {
+    float error = 0.0f, P_error = 0.0f, I_error = 0.0f, D_error = 0.0f;
+    float P_out = 0.0f, I_out = 0.0f, D_out = 0.0f;
+
     /* dt 계산 (config.h의 LOOP_TIME 기반) */
     unsigned long now = micros();
-    float dt = (float)(now - last_time) / 1000000.0f;
+    float dt = (float)(now - last_time_roll) / 1000000.0f;
     if (dt > 0.02f) dt = DT;  /* 250Hz 제한 */
     
     /* 1. 오차 계산 */
@@ -31,6 +34,7 @@ float calcPID_Roll(float targetRoll, float currentRoll) {
     roll_int += error * dt;
     if (roll_int > 50.0f) roll_int = 50.0f;      // 상한
     if (roll_int < -50.0f) roll_int = -50.0f;    // 하한
+    if(throttle<1050.0f) roll_int=0.0f;
     I_error = roll_int;
     I_out = R_I * I_error;
     
@@ -46,13 +50,15 @@ float calcPID_Roll(float targetRoll, float currentRoll) {
     if (output > 500.0f) output = 500.0f;
     if (output < -500.0f) output = -500.0f;
     
-    last_time = now;
+    last_time_roll = now;
     return output;
 }
 
-float calcPID_Pitch(float targetPitch, float currentPitch) {
+float calcPID_Pitch(float targetPitch, float currentPitch,float throttle) {
+    float error = 0.0f, P_error = 0.0f, I_error = 0.0f, D_error = 0.0f;
+    float P_out = 0.0f, I_out = 0.0f, D_out = 0.0f;
     unsigned long now = micros();
-    float dt = (float)(now - last_time) / 1000000.0f;
+    float dt = (float)(now - last_time_pitch) / 1000000.0f;
     if (dt > 0.02f) dt = DT;
     
     error = targetPitch - currentPitch;
@@ -63,6 +69,8 @@ float calcPID_Pitch(float targetPitch, float currentPitch) {
     pitch_int += error * dt;
     if (pitch_int > 50.0f) pitch_int = 50.0f;
     if (pitch_int < -50.0f) pitch_int = -50.0f;
+    if(throttle<1050.0f) pitch_int=0.0f;
+
     I_error = pitch_int;
     I_out = P_I * I_error;
     
@@ -76,13 +84,15 @@ float calcPID_Pitch(float targetPitch, float currentPitch) {
     if (output > 500.0f) output = 500.0f;
     if (output < -500.0f) output = -500.0f;
     
-    last_time = now;
+    last_time_pitch = now;
     return output;
 }
 
-float calcPID_Yaw(float targetYaw, float currentYaw) {
+float calcPID_Yaw(float targetYaw, float currentYaw,float throttle) {
+    float error = 0.0f, P_error = 0.0f, I_error = 0.0f, D_error = 0.0f;
+    float P_out = 0.0f, I_out = 0.0f, D_out = 0.0f;
     unsigned long now = micros();
-    float dt = (float)(now - last_time) / 1000000.0f;
+    float dt = (float)(now - last_time_yaw) / 1000000.0f;
     if (dt > 0.02f) dt = DT;
     
     error = targetYaw - currentYaw;
@@ -93,6 +103,8 @@ float calcPID_Yaw(float targetYaw, float currentYaw) {
     yaw_int += error * dt;
     if (yaw_int > 50.0f) yaw_int = 50.0f;
     if (yaw_int < -50.0f) yaw_int = -50.0f;
+    if(throttle<1050.0f) yaw_int=0.0f;
+
     I_error = yaw_int;
     I_out = Y_I * I_error;
     
@@ -106,7 +118,7 @@ float calcPID_Yaw(float targetYaw, float currentYaw) {
     if (output > 500.0f) output = 500.0f;
     if (output < -500.0f) output = -500.0f;
     
-    last_time = now;
+    last_time_yaw = now;
     return output;
 }
 
